aba984ea587b54ce25dfbf07f6c28d52a5e37dab
MyMetric - Marketing Analytics <36673756+mymetric@users.noreply.github.com>
2024-10-07 12:29:48 +0000

    novos usuários

--- users.py
+++ users.py
@@ -1,8 +1,14 @@
 # users.py
 
 # Dicionário de usuários e senhas
+# https://mymetric.streamlit.app
+# usuário: evoke
+# senha: asfGjHFzzAIwao8
+
 users = {
     "gringa": "zK2dyTlo7WC7Wrb",
     "oculosshop": "LhMbxYNUPEBTGrt",
+    "evoke": "asfGjHFzzAIwao8",
+    "hotbuttered": "o4LPD5CW25vQA5c",
     "universomaschio": "IMAsGYPAx3iUObX",
 }
